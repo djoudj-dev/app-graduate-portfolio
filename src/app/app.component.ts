6385eea4c707858236c752e6d641246db9dfc0bd
djoudj-dev <nedellec.julien.78@gmail.com>
2025-01-29 16:58:09 +0100

    fix(lint/prettier): corrige les erreurs de linting et de formatage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'app-graduate-portfolio';
+  title = "app-graduate-portfolio";
 }
