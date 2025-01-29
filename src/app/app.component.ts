16eac4ba32cde1b672e5721e8de625ab3d2872dd
djoudj-dev <nedellec.julien.78@gmail.com>
2025-01-29 22:01:25 +0100

    feat(app): intégrer Navbar et RouterOutlet
    - Ajout du composant `NavbarSmartComponent` et de `RouterOutlet` dans `AppComponent` pour améliorer la structure et la navigation.
    - Mise à jour du template HTML de `AppComponent` pour inclure `<app-navbar />` et `<router-outlet />`.
    - Harmonisation des guillemets simples dans tous les fichiers pour garantir une cohérence de style.
    - Adaptation des spécifications des tests unitaires suite aux changements dans la structure du DOM.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarSmartComponent } from './core/components/navbar/navbar.smart.component';
 
 @Component({
-  selector: "app-root",
-  imports: [],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarSmartComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = "app-graduate-portfolio";
+  title = 'app-graduate-portfolio';
 }
