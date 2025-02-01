c0a268f5cd48aebae613bc11c6b8a67469125cd4
djoudj-dev <nedellec.julien.78@gmail.com>
2025-02-01 12:29:31 +0100

    feat(about): refactor le composant About avec des données dynamiques et des boutons interactifs

---
+++ src/app/visitors/home/about/data/about.mock.ts
@@ -0,0 +1,18 @@
+import { About } from '../models/about.model';
+
+export const ABOUT_MOCK: About = {
+  content: {
+    title: 'Nédellec Julien',
+    subtitle: "Concepteur & Développeur d'Applications Web | Angular & Node.js",
+    description:
+      'Développeur web spécialisé en Angular et Node.js, je conçois et développe des solutions performantes, évolutives et adaptées aux besoins spécifiques de chaque projet.',
+    quote:
+      "Issu de la chaudronnerie, j'ai appris à travailler avec rigueur et précision, des qualités que j'applique aujourd'hui au développement web. Curieux et en apprentissage constant, je construis des applications web en mettant l'accent sur la performance et l'adaptabilité. Mon objectif est de concevoir des solutions efficaces tout en continuant à monter en compétences et à explorer de nouvelles technologies.",
+    quoteFooter: 'Mon parcours de reconversion professionnelle'
+  },
+  socialLinks: {
+    cv: '/assets/cv.pdf',
+    github: 'https://github.com/votre-username'
+  },
+  profileImage: '/images/about/LinkedInPhotoProfil.webp'
+};
