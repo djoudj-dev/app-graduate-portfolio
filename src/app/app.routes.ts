182d3d6242e4d0e7160807e1a83825c55dd7f8e6
djoudj-dev <nedellec.julien.78@gmail.com>
2025-02-01 21:18:11 +0100

    feat(favicon): ajouter un favicon SVG et mettre à jour le titre du document

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './visitors/home/home.page.component';
 
-export const routes: Routes = [{ path: '', component: HomePageComponent, title: 'Portfolio' }];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent, title: 'Portfolio de Djoudj' }
+];
