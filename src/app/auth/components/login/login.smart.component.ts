9402ba4153299fda668699cc62adeea8a500864e
djoudj-dev <nedellec.julien.78@gmail.com>
2025-02-03 09:27:49 +0100

    feat(login): ajouter un composant de connexion avec validation et gestion de l'état

---
+++ src/app/auth/components/login/login.smart.component.ts
@@ -0,0 +1,63 @@
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [NgClass, ReactiveFormsModule],
+  templateUrl: './login.smart.component.html',
+  styleUrl: './login.smart.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginSmartComponent {
+  @Input() isOpen = false;
+  @Output() isOpenChange = new EventEmitter<boolean>();
+
+  protected loginForm: FormGroup;
+  protected isSubmitting = false;
+  protected showPassword = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  protected isFieldInvalid(field: string): boolean {
+    const formField = this.loginForm.get(field);
+    return formField ? formField.invalid && (formField.dirty || formField.touched) : false;
+  }
+
+  protected togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  protected closeModal(): void {
+    this.isOpen = false;
+    this.isOpenChange.emit(false);
+  }
+
+  protected async onSubmit(): Promise<void> {
+    if (this.loginForm.valid) {
+      this.isSubmitting = true;
+      try {
+        // Ici, vous ajouterez la logique d'authentification
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulation
+        console.log('Form submitted:', this.loginForm.value);
+      } catch (error) {
+        console.error('Error submitting form:', error);
+      } finally {
+        this.isSubmitting = false;
+      }
+    } else {
+      Object.keys(this.loginForm.controls).forEach((key) => {
+        const control = this.loginForm.get(key);
+        if (control) {
+          control.markAsTouched();
+        }
+      });
+    }
+  }
+}
