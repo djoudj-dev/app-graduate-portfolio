50fb953ac4aaf7fb232cf6d7633be7a9a2b7bdc6
djoudj-dev <nedellec.julien.78@gmail.com>
2025-02-10 21:33:25 +0100

    feat(about): add interaction tracking for CV and GitHub links
    - Integrate CountersService to track CV and GitHub link interactions
    - Increment counters when CV is downloaded or GitHub profile is opened

--- src/app/visitors/home/about/about.dumb.component.ts
+++ src/app/visitors/home/about/about.dumb.component.ts
@@ -1,5 +1,6 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { CountersService } from '../../../admin/stats/counters/services/counters.service';
 import { ButtonDumbComponent } from '../../../shared/components/button/button.dumb.component';
 import { AboutService } from './services/about.service';
 
@@ -13,7 +14,7 @@ import { AboutService } from './services/about.service';
 export class AboutDumbComponent {
   private readonly aboutService = inject(AboutService);
   private readonly about = this.aboutService.getAbout();
-
+  private readonly countersService = inject(CountersService);
   // Computed signals basés sur about
   protected readonly content = computed(() => this.about().content);
   protected readonly socialLinks = computed(() => this.about().socialLinks);
@@ -36,10 +37,12 @@ export class AboutDumbComponent {
 
   // Méthodes pour gérer les clics
   onDownloadCV(): void {
+    this.countersService.incrementCv();
     window.open(this.socialLinks().cv, '_blank');
   }
 
   onOpenGitHub(): void {
+    this.countersService.incrementGithub();
     window.open(this.socialLinks().github, '_blank');
   }
 }
